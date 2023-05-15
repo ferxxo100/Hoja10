14eccb9998a6b55920bfefbf15d27978e21e16d7
Geraxxx5 <gerax.pineda@gmail.com>
2023-05-14 21:10:06 -0600

    The graph class was created with the Floyd algorithm, and the main class was modified.

---
+++ src/Graph.java
@@ -0,0 +1,149 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Graph {
+    Vector<Vector<Object>> arrivalRoutes = new Vector<>();
+    Vector<Vector<Object>> floydVector = new Vector<>();
+    Scanner sc;
+
+    public Graph(){
+        this.sc = new Scanner(System.in);
+    }
+
+
+
+    /**
+     * After reading the file and creating the matrix where the weather times will be stored,
+     * the vector is created where the user selects the times they want for each city.
+     * @param initialVector
+     * @param kValue
+     * @return finalVector
+     */
+    public Vector<Vector<Object>> makeTheFistVector(Vector<Vector<Object>> initialVector, HashMap<String,Integer> kValue){
+        Vector<Vector<Object>> finalVector =  new Vector<Vector<Object>>(); //Create vector
+        for(int i=0;i<initialVector.size();i++){
+            Vector<Object> startCity = initialVector.get(i);
+            Vector<Object> NstartCity = new Vector<>();
+            //Create the vector where the user will select the type of weather they want
+            int cont = 0;
+            for(int j=0;j<startCity.size();j++){
+                if(cont == i){
+                    NstartCity.add(0);
+                }else{
+                    Object element = startCity.get(j);
+                    //If the element is a list, it means that the user needs to be asked what they want here.
+                    if(element instanceof List){
+                        List<String> tiposClima = (List<String>) element;
+                        System.out.println("De "+SupportFunctions.getKeyfromValue(kValue,i)+" a "+SupportFunctions.getKeyfromValue(kValue,j)+" que clima hay?");
+                        System.out.println("0) Clima Normal\n1) Con lluvia\n2) Con Nieve\n3) Con Tormenta");
+                        String opc = sc.nextLine();
+                        int clima = 0;
+                        if(opc.equals("0") || opc.equals("1") || opc.equals("2") || opc.equals("3")){
+                            clima = Integer.parseInt(opc);
+                        }
+                        NstartCity.add(Integer.parseInt(tiposClima.get(clima)));
+                    }else{
+                        //If it's not a list, it means there's no path
+                        NstartCity.add("NE");
+                    }
+                }
+                cont++;
+            }
+            //Add vector to create the new matrix
+            finalVector.add(NstartCity);
+        }
+        for(int l = 0; l<initialVector.size();l++){
+            System.out.println(SupportFunctions.getKeyfromValue(kValue,l)+": "+finalVector.get(l));
+        }
+        return finalVector;
+    }
+
+    /**
+     * Using the Floyd algorithm, two new matrices are created.
+     * One stores the distance of the shortest routes,
+     * and the other stores the names of the cities that must be traversed to reach the destination
+     * by the shortest distance.
+     *
+     * @param finalVector
+     * @param kValue
+     * @return
+     */
+    public Vector<Vector<Object>> FloydAlgorithm(Vector<Vector<Object>> finalVector, HashMap<String,Integer> kValue){
+        Vector<Vector<Object>> shortRoute =  new Vector<Vector<Object>>();
+        Vector<Vector<Object>> cities =  new Vector<Vector<Object>>(); //Create the matrix for the routes between the cities.
+        int cont = 0;
+        for(int i = 0;i<finalVector.size();i++){
+            //Create a copy to not affect the original matrix
+            Vector<Object> ci = new Vector<>();
+            for(int j=0;j<kValue.size();j++){
+                if(cont==j){
+                    ci.add(0);
+                }else{
+                    ci.add(SupportFunctions.getKeyfromValue(kValue,j));
+                }
+            }
+            cont++;
+            cities.add(ci);
+        }
+
+        for (int i = 0;i<finalVector.size();i++){
+            Vector<Object> destino = finalVector.get(i);
+            Vector<Object> copia = new Vector<>();
+            for(int j=0;j<destino.size();j++){
+                copia.add(destino.get(j));
+            }
+            shortRoute.add(copia);
+        }
+
+        System.out.println(shortRoute);
+
+        System.out.println(cities);
+
+        //Floyd
+        for(int k=0;k<kValue.size();k++){
+            for(int i=0;i<kValue.size();i++){
+                if(k != i){
+                    for(int j=0;j<kValue.size();j++){
+                        if(k != j){
+                            if(!(shortRoute.get(i).get(k).equals("NE"))){
+                                int n1 = (int) shortRoute.get(i).get(k);
+                                if(!(shortRoute.get(k).get(j).equals("NE"))){
+                                    int n2 = (int) shortRoute.get(k).get(j);
+                                    int sum = n1+n2;
+                                    if((shortRoute.get(i).get(j).equals("NE"))){
+                                        shortRoute.get(i).set(j,sum);
+                                        cities.get(i).set(j,SupportFunctions.getKeyfromValue(kValue,k));
+                                    } else if (sum < (int)(shortRoute.get(i).get(j))) {
+                                        shortRoute.get(i).set(j,sum);
+                                        cities.get(i).set(j,SupportFunctions.getKeyfromValue(kValue,k));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            System.out.println("K = "+k);
+            System.out.println(shortRoute);
+            System.out.println(cities);
+            System.out.println("------\n");
+        }
+        System.out.println(shortRoute);
+        System.out.println(cities);
+        arrivalRoutes = cities;
+        floydVector = shortRoute;
+        return floydVector;
+    }
+
+    public Vector<Vector<Object>> getFloydVector(){
+        return this.floydVector;
+    }
+
+    public Vector<Vector<Object>> getArrivalRoutes(){
+        return this.arrivalRoutes;
+    }
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,74 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al programa");
+        //---------------
+        Scanner sc = new Scanner(System.in);
+        Vector<Vector<Object>> initialVector =  new Vector<Vector<Object>>();
+        Vector<Vector<Object>> finalVector = new Vector<>();
+        HashMap<String,Integer> kValue = new HashMap<>();
+        Graph graph = new Graph();
+        boolean flag = true;
+        String regex = "\\d+";
+        //-----Read and create variables---------
+        Reader reader = new Reader(initialVector,kValue);
+        //--------------------------------------
+        finalVector = graph.makeTheFistVector(initialVector,kValue);
+        //--------
+        while(flag){
+            //-------------------OPC---------------------
+            System.out.println("Ingrese la opcion que quiere");
+            System.out.println("0) Salir\n1) Distancia mas corta entre dos ciudades\n2) Ciudad centro del grafo" +
+                    "\n3) Modificar un grafo\n4) Indicar un clima entre ciudades");
+            String opc = sc.nextLine();
+            //--------------------------------------------
+            if(opc.equals("0")){
+                flag = false;
+            } else if (opc.equals("1")) {
+                //------------start City----------------------
+                System.out.println("Ingrese de la ciudad de la que sale");
+                for(int i=0;i<kValue.size();i++){
+                    System.out.println(i+") "+SupportFunctions.getKeyfromValue(kValue,i));
+                }
+                String sCity = sc.nextLine();
+                //------------final City---------------------
+                System.out.println("Ingrese a la ciudad a la que quiere llegar");
+                for(int i=0;i<kValue.size();i++){
+                    System.out.println(i+") "+SupportFunctions.getKeyfromValue(kValue,i));
+                }
+                String fCity = sc.nextLine();
+                //------------------------------------------
+                if(sCity.matches(regex)){
+                    if(fCity.matches(regex)){
+                        int sCityN = Integer.parseInt(sCity);
+                        int fCityN = Integer.parseInt(fCity);
+                        //------------------------------------------------
+                        if((sCityN >= 0 && sCityN<kValue.size()) && (fCityN >= 0 && fCityN<kValue.size())){
+                            if(graph.getFloydVector().get(sCityN).get(fCityN).equals("NE")){
+                                System.out.println("No hay manera de llegar a el destino");
+                            }else{
+                                System.out.println("Valor de la ruta mas corta: "+graph.getFloydVector().get(sCityN).get(fCityN));
+                                System.out.println("Recorrido de las ciudades");
+                                System.out.println(SupportFunctions.getKeyfromValue(kValue,sCityN));
+                                System.out.println(graph.arrivalRoutes.get(sCityN).get(fCityN));
+                                System.out.println(SupportFunctions.getKeyfromValue(kValue,fCityN));
+                            }
+                        }
+                        //------------------------------------------------------
+                    }
+                }
+                //------------------------------------------
+            }else if(opc.equals("2")){
+
+            }else if(opc.equals("3")){
+
+            }else if(opc.equals("4")){
+
+            }
+        }
+    }
+}
